ab2c7ee2bb15bd832295552e78fa817429a8dc20
Rimesh <Rimeshsapkota12345@gmail.com>
2021-08-06 16:41:33 +0545

    change in whole project

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css'
+const Navbar = () =>{
+    return (
+        <>
+<nav className="navbar ">
+  <a className="navbar-brand" href="#">
+      <h5>ToDoList</h5>
+  </a>
+</nav>
+        </>
+    )
+}
+export default Navbar;
---
+++ src/Component/body/Section.jsx
@@ -0,0 +1,70 @@
+import "./Section.css"
+import React from "react";
+const { useState } = React;
+const AddTaskForm = ({ addTask }) => {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        value && addTask(value)
+        setValue("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={value}
+                placeholder="Enter a title for this task…"
+                onChange={e => setValue(e.target.value)}
+            />
+            <button type="submit"><i class="fa fa-plus"></i></button>
+        </form>
+    );
+}
+
+const ToDoList = () => {
+
+    const [tasks, setTasks] = useState([{
+        text: "TO",
+        isCompleted: false
+    }, {
+        text: "DO",
+        isCompleted: false
+    }, {
+        text: "LIST",
+        isCompleted: false
+    }]);
+
+    const addTask = text => setTasks([...tasks, { text }]);
+
+    const toggleTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].isCompleted = !newTasks[index].isCompleted;
+        setTasks(newTasks);
+    };
+
+    const removeTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className="todo-list">
+            {tasks.map((task, index) => (
+
+                <div className="todo">
+                    <span onClick={() => toggleTask(index)} className={task.isCompleted ? "todo-text todo-completed" : "todo-text"}>
+                        {task.text}
+                    </span>
+                    <button onClick={() => removeTask(index)}><i class="fa fa-trash-o"></i></button>
+                </div>
+            ))}
+            <AddTaskForm addTask={addTask} />
+        </div>
+    );
+}
+
+// ReactDOM.render(<ToDoList />, document.getElementById('App'));
+export { ToDoList };
